7f38bf3aa371db2b12fbf6c739da03f50eaf1956
Du <1047944302@qq.com>
2024-09-15 11:09:41 +0800

    新增员工功能开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
 public interface EmployeeService {
+
+
 
     /**
      * 员工登录
@@ -11,5 +14,5 @@ public interface EmployeeService {
      * @return
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
-
+    void save(EmployeeDTO employeeDTO);
 }
